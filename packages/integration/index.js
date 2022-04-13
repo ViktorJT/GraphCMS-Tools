f99de35d38cb4a9eeb06e36806d7174d7a295e54
ViktorJT <v.jensentorp@gmail.com>
2022-04-13 16:00:10 +0200

    finish exporter

--- packages/integration/index.js
+++ packages/integration/index.js
@@ -1,11 +1,15 @@
 import {exportData} from '@graphcms-tools/exporter';
+import {writeFileSync} from 'fs';
 import 'dotenv/config';
 
-console.log(
-  'test',
-  exportData({
+const test = async () => {
+  const results = await exportData({
     contentApi: process.env.GRAPHCMS_CONTENT_API,
     projectId: process.env.GRAPHCMS_PROJECT_ID,
     permanentAccessToken: process.env.GRAPHCMS_PERMANENT_ACCESS_TOKEN,
-  })
-);
+  });
+
+  writeFileSync('results.json', JSON.stringify(results, null, 2));
+};
+
+test();
