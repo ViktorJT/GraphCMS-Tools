562136d535475475350580bc2e07b14d3501da21
Viktor Jensen-Torp <v.jensentorp@gmail.com>
2022-05-04 09:40:14 +0200

    add integration test

---
+++ packages/integration/main.js
@@ -0,0 +1,30 @@
+import {exportData} from '@graphcms-tools/exporter';
+// import {importData} from '@graphcms-tools/importer';
+// import {publishData} from '@graphcms-tools/publisher';
+import {writeFileSync} from 'fs';
+import 'dotenv/config';
+
+const megaTest = async () => {
+  const content = await exportData({
+    contentApi: process.env.GRAPHCMS_CONTENT_API,
+    projectId: process.env.GRAPHCMS_PROJECT_ID,
+    permanentAccessToken: process.env.GRAPHCMS_PERMANENT_ACCESS_TOKEN,
+  });
+  writeFileSync('./exportData.json', JSON.stringify(content, null, 2));
+
+  // const [imported, rejected] = await importData(content.slice(0, 10), {
+  //   contentApi: process.env.GRAPHCMS_CONTENT_API,
+  //   projectId: process.env.GRAPHCMS_PROJECT_ID,
+  //   permanentAccessToken: process.env.GRAPHCMS_PERMANENT_ACCESS_TOKEN,
+  // });
+  // writeFileSync('./importData.json', JSON.stringify([imported, rejected], null, 2));
+
+  // const [published, unpublished] = await publishData(imported.slice(0, 10), {
+  //   contentApi: process.env.GRAPHCMS_CONTENT_API,
+  //   projectId: process.env.GRAPHCMS_PROJECT_ID,
+  //   permanentAccessToken: process.env.GRAPHCMS_PERMANENT_ACCESS_TOKEN,
+  // });
+  // writeFileSync('./publishData.json', JSON.stringify([published, unpublished], null, 2));
+};
+
+megaTest();
